0985ee9c072cfc369f05f44d76f73cb4c46be1f7
Wesley Runnels <wrunnels@mit.edu>
2017-10-24 03:02:51 -0400

    classification1 edits

--- hw2_resources_2/classification1.py
+++ hw2_resources_2/classification1.py
@@ -1,14 +1,47 @@
 from numpy import *
+from
+from Problem21 import run_SVM_kernel, linear_kernel, RBF_kernel
 # import data
 train_1 = loadtxt('data/mnist_digit_1.csv')
 train_7 = loadtxt('data/mnist_digit_7.csv')
 X_1 = train_1[:500]
 X_7 = train_7[:500]
-train_X = X_1[:200] + X_7[:200]
+train_X = (X_1[:200] + X_7[:200])*2/255 - 1
 train_Y = [1 for i in range(200)] + [-1 for i in range(200)]
 
-validate_X = X_1[200:350] + X_7[200:350]
+validate_X = (X_1[200:350] + X_7[200:350])*2/255 - 1
 validate_Y = [1 for i in range(150)] + [-1 for i in range(150)]
 
-test_X = X_1[350:500] + X_7[350:500]
-test_y = [1 for i in range(150)] + [-1 for i in range(150)]
+test_X = (X_1[350:500] + X_7[350:500])*2/255 - 1
+test_Y = [1 for i in range(150)] + [-1 for i in range(150)]
+
+
+C = 1
+alpha_threshold = 10**(-8)
+kernel_func = linear_kernel
+# kernel_func = RBF_kernel(0.1)
+w0, wx_product, sv_indices, etas, alphas, savedX, savedY = run_SVM_kernel(C, alpha_threshold, train_X, train_Y, None, kernel_func)
+
+print("W0: ", w0)
+
+alphas_indices_nonzero = []
+
+for i in range(savedY.shape[0]):
+    if alphas[i] != 0:
+        alphas_indices_nonzero.append(i)
+
+def predictSVM(x):
+    # return np.array([-1 if sum([alphas[n,0] * savedY[n, 0] * kernel_func(el, savedX[n,:]) for n in range(savedY.shape[0])]) + w0 <= 0 else 1 for el in x])
+    return np.array([-1 if sum([alphas[n, 0] * savedY[n, 0] * kernel_func(el, savedX[n, :]) for n in alphas_indices_nonzero]) + w0 <= 0 else 1 for el in x])
+# plot training results
+plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
+
+
+print '======Validation======'
+# load data from csv files
+validate = loadtxt('data/data'+name+'_validate.csv')
+X = validate[:, 0:2]
+Y = validate[:, 2:3]
+# plot validation results
+plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
+pl.show()
--- hw2_resources_2/svm_test.py
+++ hw2_resources_2/svm_test.py
@@ -12,11 +12,6 @@ train = loadtxt('data/data'+name+'_train.csv')
 # use deep copy here to make cvxopt happy
 X = train[:, 0:2].copy()
 Y = train[:, 2:3].copy()
-# X, Y = np.array([[2,2],[2,3],[0,-1],[-3,-2]]), np.array([[1],[1],[-1],[-1]])
-
-
-# print X
-# print(Y.shape)
 
 # Carry out training, primal and/or dual
 C = 1
@@ -24,7 +19,6 @@ alpha_threshold = 10**(-4)
 kernel_func = RBF_kernel(.1)
 # kernel_func = linear_kernel
 w0, wx_product, sv_indices, etas, alphas, savedX, savedY = run_SVM_kernel(C, alpha_threshold, X, Y, None, kernel_func)
-# w0, w, sv_indices, etas, alphas, savedX, savedY = run_SVM(C, alpha_threshold, X, Y)
 print("W0: ", w0)
 
 alphas_indices_nonzero = []
@@ -33,16 +27,8 @@ for i in range(savedY.shape[0]):
     if alphas[i] != 0:
         alphas_indices_nonzero.append(i)
 
-# Define the predictSVM(x) function, which uses trained parameters
-# def predictSVM(x):
-#     return -1 if np.dot(w, x) + w0 <= 0 else 1
-
-# def predictSVM(x):
-#     return -1 if sum([Y[n, 0] * kernel_func(x, X[n,:]) for n in range(Y.shape[0])]) + w0 <= 0 else 1
-
 # Vectorized
 def predictSVM(x):
-    # return np.array([-1 if sum([alphas[n,0] * savedY[n, 0] * kernel_func(el, savedX[n,:]) for n in range(savedY.shape[0])]) + w0 <= 0 else 1 for el in x])
     return np.array([-1 if sum([alphas[n, 0] * savedY[n, 0] * kernel_func(el, savedX[n, :]) for n in alphas_indices_nonzero]) + w0 <= 0 else 1 for el in x])
 # plot training results
 plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
